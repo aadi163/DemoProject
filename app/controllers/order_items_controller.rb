4054abf61bcb3777c9853157792b1c25cb7ca5dc
aditya <adityarathore1122@gmail.com>
2024-08-14 17:24:53 +0530

    used before_action for login before ordering

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,10 +1,10 @@
 class OrderItemsController < ApplicationController
-
+  before_action :require_login , only: [:orderitem]
+  
   def finalorder
     @product = Product.find(params[:id])
     @useraddresses = Useraddress.all
   end
-
 
   def orderitem
     order = current_user.order || current_user.create_order
@@ -20,13 +20,22 @@ class OrderItemsController < ApplicationController
   end
 
   def destroy 
-    @order = OrderItem.find(params[:id])
-    if @order.destroy
+    @orderitem = OrderItem.find(params[:id])
+    if @orderitem.destroy
       redirect_to orders_path
     end
   end  
 
   private
-
   
+  def require_login
+    if !user_signed_in?
+      redirect_to new_user_registration_path
+
+      if user_signed_in?
+        redirect_to finalorder_path
+      end
+    end
+  end
+
 end
