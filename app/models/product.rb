c8c427cc29724541846bab7710a885827b27afc8
aditya <adityarathore1122@gmail.com>
2024-08-14 17:12:19 +0530

    used dependent: :destroy for deleting products and category

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,9 @@ class Product < ApplicationRecord
   has_one_attached :product_image
   belongs_to :subcategory
   
-  has_many :cartdataitems
+  has_many :cartdataitems , dependent: :destroy
   has_many :carts , :through => :cartdataitems
 
-  has_many :order_items
+  has_many :order_items , dependent: :destroy
   has_many :orders , :through => :order_items
 end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,4 +1,4 @@
 class Subcategory < ApplicationRecord
-  has_many :products
+  has_many :products , dependent: :destroy
   belongs_to :category
 end
