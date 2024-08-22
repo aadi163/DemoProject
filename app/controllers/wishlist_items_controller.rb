41631dee266a9301d41a944a396a9187c6d28eef
aditya <adityarathore1122@gmail.com>
2024-08-22 15:21:00 +0530

    used before_action

--- app/controllers/wishlist_items_controller.rb
+++ app/controllers/wishlist_items_controller.rb
@@ -1,13 +1,28 @@
 class WishlistItemsController < ApplicationController
+  before_action :find_id  , only: [:addtowishlist , :destroy]
+  before_action :authenticate_user! , only: [:addtowishlist]
+
+
   def addtowishlist
     wishlist = current_user.wishlist || current_user.create_wishlist
-    product = Product.find(params[:id])
-    @wishlist_item = wishlist.wishlist_items.find_or_initialize_by(product: product)
+    @wishlist_item = wishlist.wishlist_items.find_or_initialize_by(product: @product)
 
     if @wishlist_item.save
       redirect_to wishlists_path
     else
       redirect_to product_path
     end
+  end
+
+  def destroy
+    if @product.destroy
+      redirect_to wishlists_path
+    end
+  end
+
+  private
+
+  def find_id
+    @product = Product.find(params[:id])
   end
 end
