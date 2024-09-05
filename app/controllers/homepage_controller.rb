ed076803b1f19a477fe78308bc42e4647f0e22ce
aditya <adityarathore1122@gmail.com>
2024-09-05 17:52:07 +0530

    Created search feature

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,13 +1,27 @@
 class HomepageController < ApplicationController
+  before_action :all_category
 
   def index
-    @categories = Category.all
     @products = Product.all
   end
 
   def filter_products
-    @categories = Category.all
     subcategory = Subcategory.find(params[:id])
     @categoryProducts = subcategory.products.all
   end 
+
+  def search_products
+    if subcategory = Subcategory.find_by(name: params[:name]).present?
+      subcategory = Subcategory.find_by(name: params[:name])
+      @categoryProducts = subcategory.products.all
+    else
+      redirect_to homepage_path
+    end
+  end
+
+  private
+
+  def all_category
+    @categories = Category.all
+  end
 end
