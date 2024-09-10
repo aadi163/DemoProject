796bb78b809896085ee1768a701b4a201c9751d2
aditya <adityarathore1122@gmail.com>
2024-09-10 19:51:52 +0530

    Did some changes in controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user! 
 
   def index
-    order = Order.where(user_id: current_user.id)
-    @orders = OrderItem.where(order_id: order)
+    orders = Order.where(user_id: current_user.id)
+    @orders = OrderItem.where(order_id: orders.ids)
   end
 end
